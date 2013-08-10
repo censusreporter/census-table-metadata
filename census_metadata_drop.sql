8516c8ea7c69a820fd05704c4e21635414a58328
Ian Dees <ian.dees@gmail.com>
2013-08-10 11:22:51 -0500

    Add a drop script for the metadata tables.

---
+++ census_metadata_drop.sql
@@ -0,0 +1,59 @@
+DROP TABLE acs2007_1yr.census_table_metadata;
+DROP TABLE acs2007_1yr.census_column_metadata;
+DROP TABLE acs2007_1yr.census_topics;
+DROP TABLE acs2007_1yr.census_table_topics;
+
+DROP TABLE acs2007_3yr.census_table_metadata;
+DROP TABLE acs2007_3yr.census_column_metadata;
+DROP TABLE acs2007_3yr.census_topics;
+DROP TABLE acs2007_3yr.census_table_topics;
+
+DROP TABLE acs2008_1yr.census_table_metadata;
+DROP TABLE acs2008_1yr.census_column_metadata;
+DROP TABLE acs2008_1yr.census_topics;
+DROP TABLE acs2008_1yr.census_table_topics;
+
+DROP TABLE acs2008_3yr.census_table_metadata;
+DROP TABLE acs2008_3yr.census_column_metadata;
+DROP TABLE acs2008_3yr.census_topics;
+DROP TABLE acs2008_3yr.census_table_topics;
+
+DROP TABLE acs2009_1yr.census_table_metadata;
+DROP TABLE acs2009_1yr.census_column_metadata;
+DROP TABLE acs2009_1yr.census_topics;
+DROP TABLE acs2009_1yr.census_table_topics;
+
+DROP TABLE acs2009_3yr.census_table_metadata;
+DROP TABLE acs2009_3yr.census_column_metadata;
+DROP TABLE acs2009_3yr.census_topics;
+DROP TABLE acs2009_3yr.census_table_topics;
+
+DROP TABLE acs2010_1yr.census_table_metadata;
+DROP TABLE acs2010_1yr.census_column_metadata;
+DROP TABLE acs2010_1yr.census_topics;
+DROP TABLE acs2010_1yr.census_table_topics;
+
+DROP TABLE acs2010_3yr.census_table_metadata;
+DROP TABLE acs2010_3yr.census_column_metadata;
+DROP TABLE acs2010_3yr.census_topics;
+DROP TABLE acs2010_3yr.census_table_topics;
+
+DROP TABLE acs2010_5yr.census_table_metadata;
+DROP TABLE acs2010_5yr.census_column_metadata;
+DROP TABLE acs2010_5yr.census_topics;
+DROP TABLE acs2010_5yr.census_table_topics;
+
+DROP TABLE acs2011_1yr.census_table_metadata;
+DROP TABLE acs2011_1yr.census_column_metadata;
+DROP TABLE acs2011_1yr.census_topics;
+DROP TABLE acs2011_1yr.census_table_topics;
+
+DROP TABLE acs2011_3yr.census_table_metadata;
+DROP TABLE acs2011_3yr.census_column_metadata;
+DROP TABLE acs2011_3yr.census_topics;
+DROP TABLE acs2011_3yr.census_table_topics;
+
+DROP TABLE acs2011_5yr.census_table_metadata;
+DROP TABLE acs2011_5yr.census_column_metadata;
+DROP TABLE acs2011_5yr.census_topics;
+DROP TABLE acs2011_5yr.census_table_topics;
