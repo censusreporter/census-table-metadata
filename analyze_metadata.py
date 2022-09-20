5056ccc59f050aa58658cee2dd915baba217028b
Ian Dees <ian.dees@gmail.com>
2022-09-19 20:07:58 -0500

    Update unified metadata

--- analyze_metadata.py
+++ analyze_metadata.py
@@ -6,8 +6,8 @@ import os
 import re
 from collections import defaultdict
 
-releases_to_analyze = ['acs2019_1yr', 'acs2020_5yr']
-releases_to_output = ['acs2019_1yr', None, 'acs2020_5yr']
+releases_to_analyze = ['acs2021_1yr', 'acs2020_5yr']
+releases_to_output = ['acs2021_1yr', None, 'acs2020_5yr']
 tables_by_code = defaultdict(list)
 
 tables_by_release = defaultdict(dict)
